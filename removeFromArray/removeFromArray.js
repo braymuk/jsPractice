c63737287d14cbfcde83316601b683e7cd764949
braymuk <braydeng9@gmail.com>
2019-11-25 15:07:50 -0800

    Got through like 4 tests of removeFromArray with some shitty code but then hit a roadblock after that

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, pos1, pos2, pos3, pos4) {
+    let removed1 = array.splice(pos1-1,1);
+    if(pos2>0) {
+        let removed2 = array.splice(pos2-1,1);
+    }
+    if(pos3>0) {
+        let removed3 = array.splice(pos3-1,1);
+    }
+    if(pos4>0) {
+        let removed4 = array.splice(pos4-1,1);
+    }
 
+
+    return array;
 }
 
 module.exports = removeFromArray
