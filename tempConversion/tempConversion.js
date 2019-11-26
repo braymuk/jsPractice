432e6ddbbecb7e02f7c475209d83a5e2970f67f7
braymuk <braydeng9@gmail.com>
2019-11-26 14:35:58 -0800

    Finished tempConversion.js practice

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  raw = (temp-32)*(5/9)
+  return Math.round(raw*10)/10;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  raw = (temp)*(9/5)+32
+  return Math.round(raw*10)/10;
 }
 
 module.exports = {
