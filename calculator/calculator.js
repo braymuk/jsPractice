ace489efcf4069367f3243dbf05a90b873aae20f
braymuk <braydeng9@gmail.com>
2020-01-11 15:46:20 -0800

    Finished Calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,24 +1,55 @@
 function add () {
-	
+	var sum = 0;
+	for(var i=0;i<arguments.length;i++){
+		sum+= arguments[i];
+	}
+	return sum;
 }
 
 function subtract () {
-	
+	var value = arguments[0]-arguments[1];
+	return value;
 }
 
-function sum () {
-	
-}
+function sum (arr) {
+		return arr.reduce(function(a,b){
+		  return a + b
+		}, 0);
+	};
 
-function multiply () {
+
+function multiply (arr) {
+	var sum = arr[0];
+	for(i=1;i<arr.length;i++) {
+		sum *= arr[i];
+	}
+	return sum;
 	
 }
 
 function power() {
+	var base = arguments[0];
+	var power = arguments[1];
+	var one = 1;
+	for(i=0;i<power;i++) {
+		one*= base;
+	}
+	return one;
 	
 }
 
-function factorial() {
+function factorial(num) {
+	var one = 1;
+	if (num===0) {
+		return 1;
+	} else {
+		var number = num;
+		for (i=1;i<num;i++) {
+			number *= num-i;
+		}
+		return number;
+
+	}
 	
 }
 
