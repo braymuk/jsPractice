981aa115a75f37391b9b1671a2605da7914e7b11
braymuk <braydeng9@gmail.com>
2021-07-01 21:26:46 -0700

    Completed fibonacci

--- exercises/fibonacci/fibonacci.js
+++ exercises/fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    console.log(index);
 
+    if(index <= 0) {
+        return "OOPS";
+    }
+    if(index === 1) {
+        return 1;
+    } else if (index === 2) {
+        return 1;
+    }
+
+    return fibonacci(index-1) + fibonacci(index - 2);
 }
 
 module.exports = fibonacci
