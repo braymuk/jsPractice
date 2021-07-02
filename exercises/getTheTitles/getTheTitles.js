fc4d37db28d085d38eac7a05fb8e22f9d95fcaf6
braymuk <braydeng9@gmail.com>
2021-07-01 21:33:15 -0700

    Completed getTheTitles

--- exercises/getTheTitles/getTheTitles.js
+++ exercises/getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let results = [];
+    books.map((book, index)=> {
+        results[index] = book.title;
+    })
+    return results;
 }
 
 module.exports = getTheTitles;
