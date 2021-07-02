a765b62d5004792f1183a454bda242f664936bc2
braymuk <braydeng9@gmail.com>
2021-07-01 21:56:31 -0700

    Completed findTheOldest

--- exercises/findTheOldest/findTheOldest.js
+++ exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-let findTheOldest = function() {
+let findTheOldest = function(array) {
+    let currentOldestPerson = array[0];
+    let age =0; 
 
+    array.forEach((person,index)=> {
+        if(person.yearOfDeath) {
+            age = person.yearOfDeath- person.yearOfBirth;
+        } else {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        }
+
+        if(currentOldestPerson.yearOfDeath) {
+            oldAge = currentOldestPerson.yearOfDeath- currentOldestPerson.yearOfBirth;
+        } else {
+            oldAge = new Date().getFullYear() - currentOldestPerson.yearOfBirth;
+        }
+        console.log(age);
+
+        if(age > oldAge) {
+            currentOldestPerson = person;
+        }
+        
+    })
+
+
+    return currentOldestPerson;
 }
 
 module.exports = findTheOldest
