2cd65dd0c32fca0744650ba7bd041b74173d038e
braymuk <braydeng9@gmail.com>
2021-07-01 21:17:57 -0700

    Completed palindromes

--- exercises/palindromes/palindromes.js
+++ exercises/palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function() {
+const palindromes = function(string) {
+   let reversedString = "";
+   let j = 0;
+   let punctuationless = string.replace(/[\.,-\/#!$%\^&\*;:{}" "=\-_`~()@\+\?><\[\]\+]/g, '');
+   let cleanString = punctuationless.replace(/\s{2,}/g," ");
+    cleanString = cleanString.toLowerCase();
 
+   console.log("clean string: " + cleanString);
+   for(i = cleanString.length-1; i >= 0; i--) {
+        reversedString = reversedString + cleanString[i].toLowerCase();
+       j++;
+   }
+   console.log("reversed string: " + reversedString);
+
+   if(cleanString === reversedString) {
+       return true;
+   } else {
+       return false;
+   }
 }
 
 module.exports = palindromes
